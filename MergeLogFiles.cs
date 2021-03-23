5749af3a825107ad49f216c7ec5a5df01ba81f66
Robert Faller <Robert.Faller@thedeltagroup.co.uk>
2021-03-23 22:07:46 +0000

    Initial commit

---
+++ MergeLogFiles.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace LogFileMerger
+{
+    public class MergeLogFiles
+    {
+        public static Action Merge()
+        {
+            //Defining the input folder
+            string inputFolder = "C:\\PersonalDev\\TestLogs\\";
+
+            //collecting the files
+            string[] files = Directory.GetFiles(inputFolder);
+
+            //opening the data model
+            List<Data> data = new List<Data>();
+           
+            //initiating IEnumerable of lines to store the lines from the files.
+            IEnumerable<String> lines;
+            
+            //foreach loop to loop through the files
+            foreach (var fd in files)
+            {
+                lines = File.ReadAllLines(fd);
+                //adding the data to the data model - so this can be sorted later
+                foreach (var l in lines)
+                {
+                    Data d = new Data
+                    {
+                        Date = l.Substring(0, 23),
+                        Value = l
+                    };
+                    data.Add(d);
+                }
+            }
+            //writing to the output file
+            Console.WriteLine("Writing to output file");
+            using (TextWriter tw = new StreamWriter(string.Concat(inputFolder + "\\Results\\testOutput.txt")))
+            {
+                foreach (var d in data.OrderBy(x => x.Date))
+                {
+                    tw.WriteLine(d.Value);
+                }
+            }
+            Console.WriteLine("Writing finished");
+
+            return null;
+        }
+
+        //Would normally put this into it's own model folder, but for speed, and the fact this is the only model in the program i left it here.
+        public class Data
+        {
+            public string Date { get; set; }
+
+            public string Value { get; set; }
+        }
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace LogFileMerger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting Program, lets see what happens here!");
+            MergeLogFiles.Merge();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
